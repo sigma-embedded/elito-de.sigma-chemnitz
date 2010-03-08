b9dfeef075a9b1a955992fd40034b1364c132b57
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2010-03-08 01:13:15 +0100

    pkg.mk: initial checkin of pkg handling stuff

---
+++ mk/pkg.mk
@@ -0,0 +1,28 @@
+IPKG_MAKE_INDEX =	ipkg-make-index
+IPKG =			opkg-cl
+FAKEROOT =		fakeroot
+
+_pkgs =		$(foreach a,${PACKAGE_ARCHS},${DEPLOY_DIR_IPK}/$a/Packages)
+
+pkg-regen:	$(wildcard ${_pkgs})
+	@:
+
+.PHONY:		pkg-update
+pkg-update:	${STAGING_ETCDIR_NATIVE}/opkg.conf pkg-regen
+	$(FAKEROOT) $(IPKG) -o $(DESTDIR) -f '$<' update || :
+
+.PHONY:		pkg-upgrade
+pkg-upgrade:	${STAGING_ETCDIR_NATIVE}/opkg.conf pkg-regen
+	$(FAKEROOT) $(IPKG) -o $(DESTDIR) -f '$<' update || :
+	$(FAKEROOT) $(IPKG) -o $(DESTDIR) -f '$<' upgrade
+
+.PHONY:		pkg-install
+pkg-install:	${STAGING_ETCDIR_NATIVE}/opkg.conf
+	$(FAKEROOT) $(IPKG) -o $(DESTDIR) -f '$<' install ${P}
+
+.PHONY:		pkg-remove
+pkg-remove:
+	$(FAKEROOT) $(IPKG) -o $(DESTDIR) -f '$<' remove ${P}
+
+${_pkgs}:${DEPLOY_DIR_IPK}/%/Packages:	${DEPLOY_DIR_IPK}/%
+	$(IPKG_MAKE_INDEX) -r $@ -p $@ -l $@.filelist -m $<
