c965430f8b30c2433bbdc885faa64cccbc6b5fce
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2010-05-30 15:59:27 +0200

    u-boot.mk: unset MAKEFLAGS; set some configuration parameters

--- mk/u-boot.mk
+++ mk/u-boot.mk
@@ -2,8 +2,13 @@ ENV = \
 	-u CPPFLAGS -u CFLAGS -u LDFLAGS
 
 OPTS = \
+	MAKEFLAGS= \
 	AR:='$(AR)' AS:='$(AS)' CC:='$(CC)' CPP:='$(CPP)' \
 	LD:='$(LD)' NM:='$(NM)' STRIP:='$(STRIP)' \
 	OBJCOPY:='$(OBJCOPY)' OBJDUMP:='$(OBJDUMP)' \
 	RANLIB:='$(RANLIB)' HOSTCC:='$(BUILD_CC)' \
-	HOSTCFLAGS:='$(BUILD_CFLAGS)' CROSS_COMPILE:='$(_CROSS)'
+	HOSTCFLAGS:='$(BUILD_CFLAGS)' CROSS_COMPILE:='$(_CROSS)' \
+	$(if ${_kernel_tftp_image},CFG_BOOTFILE:='$(basename $(_kernel_tftp_image))') \
+	$(if ${_tftp_server},CFG_SERVERIP='${_tftp_server}') \
+        CFG_NFSROOT='MK_STR(CONFIG_SERVERIP) ":${DESTDIR}"'
+
