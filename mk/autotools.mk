e74f1c2e6aed78729a74d75bc446515a23352218
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2010-06-08 11:35:30 +0200

    mk: added autotools, devel + eclipse plugins

---
+++ mk/autotools.mk
@@ -0,0 +1,26 @@
+CONFIGURE =	./configure
+
+_cmdline = \
+	--build=${BUILD_SYS} \
+	--host=${HOST_SYS} \
+	--target=${TARGET_SYS} \
+	PATH='${PATH}' \
+	CC='${CC}' \
+	CFLAGS='${CFLAGS}' \
+	CXX='${CXX}' \
+	CXXFLAGS='${CXXFLAGS}' \
+	CPP='${CPP}' \
+	CPPFLAGS='${CPPFLAGS}' \
+	LDFLAGS='${LDFLAGS}'
+
+printcmd:
+	@echo "${_cmdline}"
+
+configure:
+	sh ${CONFIGURE} ${_cmdline} ${C}
+
+.PHONY:	configure printcmd
+
+ifeq (${V},)
+MAKEFLAGS += " -s"
+endif
---
+++ mk/eclipse.mk
@@ -0,0 +1,48 @@
+SED ?= sed
+
+_sed_cmd = $(SED) \
+	-e 's!${STAGING_DIR_TARGET}/\+!\$${T_TARGET}/!g' \
+	-e 's!${STAGING_DIR_NATIVE}/\+!\$${T_NATIVE}/!g' \
+	-e 's!${TOOLCHAIN_PATH}/\+!\$${T_CROSS}/!g' \
+	-e 's!${_tmpdir}/!\$${T}/!g' \
+	-e 's!${_CROSS}!\$${CROSS}!g'
+
+_sed_cmd1 = $(SED) \
+	-e 's!${_tmpdir}/!\$${T}/!g' \
+
+_ign_cflags = -f% -O% -g%
+_ign_ldflags = -Wl,-O%
+
+ECLIPSE = eclipse
+export CROSS = ${_CROSS}
+ENV += "CROSS=${CROSS}"
+
+info:
+	echo "Autotools:"
+	echo "  Configure Settings"
+	echo "    Platform specifiers"
+	echo "      Host platform: ${HOST_SYS}"
+	echo "      Build platform: ${BUILD_SYS}"
+	echo "      Target platform: ${TARGET_SYS}"
+	echo "    Advanced"
+	echo "      Additional options: PATH=${PATH}" | ${_sed_cmd}
+	echo
+	echo 'C/C++ Build'
+	echo '  Build Variables'
+	echo '    CROSS: ${_CROSS}'
+	echo '    DESTDIR: ${DESTDIR}'
+	echo '    T: ${_tmpdir}'
+	echo '    T_CROSS: ${TOOLCHAIN_PATH}' | ${_sed_cmd1}
+	echo '    T_TARGET: ${STAGING_DIR_TARGET}' | ${_sed_cmd1}
+	echo '    T_NATIVE: ${STAGING_DIR_NATIVE}' | ${_sed_cmd1}
+	echo '    CC: ${CC}' | ${_sed_cmd}
+	echo '    CXX: ${CXX}' | ${_sed_cmd}
+	echo '    CFLAGS: ${filter-out ${_ign_cflags},${CFLAGS}}' | ${_sed_cmd}
+	echo '    CPPFLAGS: ${filter-out ${_ign_cflags},${CPPFLAGS}}' | ${_sed_cmd}
+	echo '    CXXFLAGS: ${filter-out ${_ign_cflags},${CXXFLAGS}}' | ${_sed_cmd}
+	echo '    LDFLAGS: ${filter-out ${_ign_ldflags},${LDFLAGS}}' | ${_sed_cmd}
+
+run:
+	env -u MAKELEVEL $(ENV) ${ECLIPSE}
+
+MAKEFLAGS += -s
