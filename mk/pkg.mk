aacea83eedc174f9f4b798b5d80470dcbfde25dc
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2011-07-20 11:19:51 +0200

    opkg.mk: updated to new layout

--- mk/pkg.mk
+++ mk/pkg.mk
@@ -6,6 +6,7 @@ _pkgs =			$(foreach a,${PACKAGE_ARCHS},${DEPLOY_DIR_IPK}/$a/Packages)
 
 _fakeroot =		$(_start) $(FAKEROOT)
 _opkg_make_index =	$(_start) $(OPKG_MAKE_INDEX)
+_opkg_conf =		${DEPLOY_DIR_IPK}/opkg.conf
 
 LOCALGOALS =		pkg-regen pkg-update pkg-upgrade \
 			pkg-install pkg-remove
@@ -14,20 +15,20 @@ pkg-regen:	$(wildcard ${_pkgs})
 	@:
 
 .PHONY:		pkg-update
-pkg-update:	${STAGING_ETCDIR_NATIVE}/opkg.conf pkg-regen
+pkg-update:	${_opkg_conf} pkg-regen
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' update || :
 
 .PHONY:		pkg-upgrade
-pkg-upgrade:	${STAGING_ETCDIR_NATIVE}/opkg.conf pkg-regen
+pkg-upgrade:	${_opkg_conf} pkg-regen
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' update || :
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' upgrade
 
 .PHONY:		pkg-install
-pkg-install:	${STAGING_ETCDIR_NATIVE}/opkg.conf pkg-update
+pkg-install:	${_opkg_conf} pkg-update
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' install ${P}
 
 .PHONY:		pkg-remove
-pkg-remove:	${STAGING_ETCDIR_NATIVE}/opkg.conf
+pkg-remove:	${_opkg_conf}
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' remove ${P}
 
 ${_pkgs}:${DEPLOY_DIR_IPK}/%/Packages:	${DEPLOY_DIR_IPK}/%
