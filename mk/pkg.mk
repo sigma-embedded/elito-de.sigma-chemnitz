8db57adab2ca2a9f9e630f854fc3965f8dc11b24
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2011-12-29 13:25:32 +0100

    pkg.mk: set env and --force_postinstall option

--- mk/pkg.mk
+++ mk/pkg.mk
@@ -1,6 +1,8 @@
 OPKG_MAKE_INDEX =	opkg-make-index
 FAKEROOT =		fakeroot
 OPKG =			opkg-cl
+OPKG_ENV =		env D='$(DESTDIR)'
+OPKG_OPTS =		--force_postinstall
 
 _pkgs =			$(foreach a,${PACKAGE_ARCHS},${DEPLOY_DIR_IPK}/$a/Packages)
 
@@ -16,24 +18,24 @@ pkg-regen:	$(wildcard ${_pkgs})
 
 .PHONY:		pkg-update
 pkg-update:	${_opkg_conf} pkg-regen
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' update || :
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) update || :
 
 .PHONY:		pkg-upgrade
 pkg-upgrade:	${_opkg_conf} pkg-regen
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' update || :
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' upgrade
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) update || :
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) upgrade
 
 .PHONY:		pkg-install
 pkg-install:	${_opkg_conf} pkg-update
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' install ${P}
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) install ${P}
 
 .PHONY:		pkg-reinstall
 pkg-reinstall:	${_opkg_conf} pkg-update
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' --force-reinstall install ${P}
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) --force-reinstall install ${P}
 
 .PHONY:		pkg-remove
 pkg-remove:	${_opkg_conf}
-	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' remove ${P}
+	$(_fakeroot) $(OPKG_ENV) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) remove ${P}
 
 ${_pkgs}:${DEPLOY_DIR_IPK}/%/Packages:	${DEPLOY_DIR_IPK}/%
 	$(_opkg_make_index) -r $@ -p $@ -l $@.filelist -m $<
