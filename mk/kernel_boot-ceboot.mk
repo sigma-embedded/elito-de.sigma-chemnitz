33fe8174d4b0a45c0173140a03930efa0d77e1dd
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2012-05-04 12:15:41 +0200

    ce-boot: updated for recent kernels

--- mk/kernel_boot-ceboot.mk
+++ mk/kernel_boot-ceboot.mk
@@ -2,7 +2,7 @@ CEBOOT_GOALS	=  ce-boot
 LOCALGOALS	+= $(CEBOOT_GOALS)
 
 ce-boot:
-	+$(_build_cmd) ${_k_all_target} ceImage LOADADDR=$(KERNEL_START_ADDRESS) STARTADDR=$(KERNEL_START_ADDRESS)
+	+$(_build_cmd) ${_k_all_target} ceImage LOADADDR=$(KERNEL_START_ADDRESS) STARTADDR=$(KERNEL_START_ADDRESS) CE_LOADADDR=$(KERNEL_START_ADDRESS)
 	-cat arch/$(ARCH)/boot/ceImage > $(TFTP_IMAGE)
 
 tftp:	ce-boot
