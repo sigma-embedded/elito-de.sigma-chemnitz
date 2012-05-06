e892bbc8e2cff32034eabf803a5931ed7ff64d96
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2012-05-06 13:12:12 +0200

    kerne-boot: replaced hardcoded 'arm' architecture with $ARCH

--- mk/kernel_boot-bzimage.mk
+++ mk/kernel_boot-bzimage.mk
@@ -3,6 +3,6 @@ LOCALGOALS	+= ${UBOOT_GOALS}
 
 tftp-bzimage:
 	+$(_build_cmd) ${_k_all_target} bzImage
-	cat arch/arm/boot/bzImage >$(TFTP_IMAGE)
+	cat arch/$(ARCH)/boot/bzImage >$(TFTP_IMAGE)
 
 tftp:	tftp-bzimage
--- mk/kernel_boot-uboot.mk
+++ mk/kernel_boot-uboot.mk
@@ -3,6 +3,6 @@ LOCALGOALS	+= ${UBOOT_GOALS}
 
 tftp-uboot:
 	+$(_build_cmd) ${_k_all_target} uImage
-	cat arch/arm/boot/uImage >$(TFTP_IMAGE)
+	cat arch/$(ARCH)/boot/uImage >$(TFTP_IMAGE)
 
 tftp:	tftp-uboot
