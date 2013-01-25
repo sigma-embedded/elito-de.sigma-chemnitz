e48f49901a7aeb6dc6c6bf24b6caa7c86b014d5a
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2013-01-25 19:54:52 +0100

    image-stream.mk: enabled zlib compression by default

--- mk/image-stream.mk
+++ mk/image-stream.mk
@@ -5,7 +5,7 @@ genopts = $(if $(SKIP_$2),,-h $1${SIGNATURE_OPTS}!${IMAGE_$2})
 -include ${ELITO_TOPDIR}/mk/image-stream_${MACHINE}.mk
 -include ${PROJECT_TOPDIR}/mk/image-stream.local
 
-SIGNATURE_OPTS ?= ,sha1
+SIGNATURE_OPTS ?= ,sha1,zlib
 LOCALGOALS =	image-stream
 
 
