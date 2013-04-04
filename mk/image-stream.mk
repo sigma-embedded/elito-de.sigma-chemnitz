f90a485d00e1cd483d4ebeb3d1a8dacfada20e1a
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2013-04-04 12:19:17 +0200

    image-stream: enhanced to allow skipping of non existing files

--- mk/image-stream.mk
+++ mk/image-stream.mk
@@ -1,7 +1,9 @@
 ####
 
-genopts = $(if $(SKIP_$2),,-h $1${SIGNATURE_OPTS}!${IMAGE_$2})
+_check_skip = $(SKIP_$1)$(if $2,$(if $(wildcard ${IMAGE_$1}),,t,),)
+genopts = $(if $(call _check_skip,$2,$3),,-h $1${SIGNATURE_OPTS}!${IMAGE_$2})
 
+-include ${ELITO_TOPDIR}/mk/image-stream_${SOC_FAMILY}.mk
 -include ${ELITO_TOPDIR}/mk/image-stream_${MACHINE}.mk
 -include ${PROJECT_TOPDIR}/mk/image-stream.local
 
