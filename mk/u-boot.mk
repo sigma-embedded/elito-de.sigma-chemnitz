45233087ce9b58ab7424ebccde13bfa41b1484a3
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2013-04-23 18:20:09 +0200

    u-boot: use =, not :=

--- mk/u-boot.mk
+++ mk/u-boot.mk
@@ -12,13 +12,13 @@ ENV = $(addprefix -u ,CPPFLAGS CFLAGS LDFLAGS \
 	obj)
 
 OPTS = \
-	CROSS_COMPILE:='$(_CROSS)' \
-	AR:='$(AR)' AS:='$(AS)' CC:='$(CC)' CPP:='$(CPP)' \
-	LD:='$(LD)' NM:='$(NM)' STRIP:='$(STRIP)' \
-	OBJCOPY:='$(OBJCOPY)' OBJDUMP:='$(OBJDUMP)' \
-	RANLIB:='$(RANLIB)' \
-	HOSTCC:='$(BUILD_CC)' HOSTSTRIP:='$(BUILD_STRIP)' \
-	$(if ${_kernel_tftp_image},CFG_BOOTFILE:='$(notdir $(_kernel_tftp_image))') \
+	CROSS_COMPILE='$(_CROSS)' \
+	AR='$(AR)' AS='$(AS)' CC='$(CC)' CPP='$(CPP)' \
+	LD='$(KERNEL_LD)' NM='$(NM)' STRIP='$(STRIP)' \
+	OBJCOPY='$(OBJCOPY)' OBJDUMP='$(OBJDUMP)' \
+	RANLIB='$(RANLIB)' \
+	HOSTCC='$(BUILD_CC)' HOSTSTRIP='$(BUILD_STRIP)' \
+	$(if ${_kernel_tftp_image},CFG_BOOTFILE='$(notdir $(_kernel_tftp_image))') \
 	$(if ${_tftp_server},CFG_SERVERIP='${_tftp_server}') \
 	CFG_NFSROOT='"${_nfs_server}:${_nfs_root}"'
 
