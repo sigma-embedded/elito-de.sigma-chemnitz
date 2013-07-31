0c1ff53092ab7f45f8f4c193c59b2451d68cb52f
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2013-07-31 11:33:00 +0200

    pkg.mk: fixed/optimized operation
    execute programs with LANG=C, do not generate package.filelist anymore
    and set OPKG_0FFLINE_ROOT
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- mk/pkg.mk
+++ mk/pkg.mk
@@ -1,7 +1,7 @@
 OPKG_MAKE_INDEX =	opkg-make-index
 FAKEROOT =		${ELITO_TOPDIR}/scripts/run-pseudo '${PROJECT_TOPDIR}'
 OPKG =			opkg-cl
-OPKG_ENV =		env D='$(DESTDIR)'
+OPKG_ENV =		env D='$(DESTDIR)' LANG=C OPKG_OFFLINE_ROOT='$(DESTDIR)'
 OPKG_OPTS =		--force_postinstall
 
 _pkgs =			$(foreach a,${PACKAGE_ARCHS},${DEPLOY_DIR_IPK}/$a/Packages)
@@ -10,7 +10,7 @@ _pkgs_archs =		$(patsubst ${DEPLOY_DIR_IPK}/%/Packages,%,${_pkgs_dirs})
 _pkgs_stamp =		$(foreach a,${_pkgs_archs},${_tmpdir}/stamps/opkg-arch_$a.stamp)
 
 _fakeroot =		$(_start) $(OPKG_ENV) $(FAKEROOT)
-_opkg_make_index =	$(_start) $(OPKG_MAKE_INDEX)
+_opkg_make_index =	$(_start) env LANG=C $(OPKG_MAKE_INDEX)
 _opkg_conf =		${DEPLOY_DIR_IPK}/opkg.conf
 
 
@@ -45,7 +45,7 @@ pkg-remove:	${_opkg_conf}
 	$(_fakeroot) $(OPKG) -o $(DESTDIR) -f '$<' $(OPKG_OPTS) remove ${P}
 
 ${_pkgs}:${DEPLOY_DIR_IPK}/%/Packages:	${DEPLOY_DIR_IPK}/%
-	$(_opkg_make_index) -r $@ -p $@ -l $@.filelist -m $<
+	$(_opkg_make_index) -r $@ -p $@ -m $<
 	test -e $@	   # ensure that target really exists after this rule
 	touch $@	   # make sure that timestamps are as expected
 
