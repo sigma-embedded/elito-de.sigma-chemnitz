ed0f14c3947b8acfb5d0607f9b2d8bf6680ab808
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2016-12-21 17:56:59 +0100

    mk: enhanced gitlab-ci.mk script
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- scripts/gitlab-ci.mk
+++ scripts/gitlab-ci.mk
@@ -1,14 +1,50 @@
-ci-build:	$(foreach p,${PROJECTS},.ci-build-$p)
-ci-clean:	$(foreach p,${PROJECTS},.ci-clean-$p)
+CI_CACHEROOT ?= /cache
+CI_SOURCEDIR  = ${CI_CACHEROOT}/sources
+CI_NFSROOT   ?= $(abspath .)/.nfs
 
-.ci-build-%:	.ci-prepare-%
+_CI_TOPFLAGS = \
+	_MODE=ci \
+	CI_NFSROOT='${CI_NFSROOT}' \
+	CI_CACHEROOT='${CI_CACHEROOT}' \
+	CI_SOURCEDIR='${CI_SOURCEDIR}' \
+
+ci-build ci-clean:
+	${MAKE_ORIG} ${_CI_TOPFLAGS} '.$@'
+
+######### {{{ _MODE == ci ########
+ifeq (${_MODE},ci)
+
+.ci-build:	$(foreach p,${PROJECTS},.ci-build-$p)
+	-${CI_DIR}/source-distribute ${CI_SOURCEDIR}
+	-${CI_DIR}/sstate-distribute ${CI_CACHEROOT}/sstate
+
+.ci-clean:	$(foreach p,${PROJECTS},.ci-clean-$p)
+
+.ci-image-%:	.ci-prepare-%
 	${MAKE} -C '$*' all-images
+
+.ci-deploy-%:	.ci-prepare-% .ci-image-%
+	${MAKE} -C '$*' ci-deploy CI_DEPLOYDIR="$(abspath .)/_deploy${CI_FLAVOR}"
+
+.ci-build-%:	.ci-image-% .ci-deploy-%
+	:
 
 .ci-clean-%:
 	${MAKE} -C '$*' mrproper
 
 .ci-prepare-%:	.ci-top-prepare %/conf/local-local.conf
-	${MAKE} configure M=$*
+	${MAKE} configure M=$* CACHEROOT='${CI_CACHEROOT}' NFSROOT='${CI_NFSROOT}'
 
 .ci-top-prepare:
 	${MAKE} prepare GIT=:
+
+%/conf/local-local.conf:	%/conf/tmpl.conf
+	@rm -f $@
+	${CI_DIR}/oe-conf "-" "${@D}/.." "$(abspath $<)" "$@" 0
+
+%/conf/tmpl.conf:
+	@rm -f $@
+	@echo 'DL_DIR = "${CI_SOURCEDIR}"'  > $@
+
+endif
+######### }}} _MODE == ci ########
